fcb6ef02fda3674fa992bb55ccb3fd1a24b009c4
Vladislav <ostrikovvlad20@gmail.com>
2019-10-06 09:48:48 -0700

    FINALE

---
+++ Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Assignment1.Models;
+
+namespace Assignment1.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Title = "Tasty Recipes";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult AddRecipe()
+        {
+            ViewBag.Title = "Add Recipe";
+            return View();
+        }
+        [HttpPost]
+        public ViewResult AddRecipe(Recipe recipe)
+        {
+            Repository.AddRecipe(recipe);
+            return View("RecipeList", Repository.Recipes);
+        }
+        
+        public ViewResult RecipeList()
+        {
+            ViewBag.Title = "Recipe List";
+            return View("RecipeList", Repository.Recipes);
+        }
+        public ViewResult ViewRecipe()
+        {
+            ViewBag.Title = "Recipe Details";
+            return View("ViewRecipe", Repository.Recipes.Where(r => r.Title == "Potato perogies"));
+        }
+    }
+}
---
+++ Assignment1/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1.Models
+{
+    public class Recipe
+    {
+        private string ingredients;
+
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public string Instruction { get; set; }
+    }
+}
